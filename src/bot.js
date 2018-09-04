b5a988fc03613e0837d6814d7ba71d6d33184114
Peter <1301peter@gmail.com>
2018-09-04 20:31:17 +0200

    tweet

--- src/bot.js
+++ src/bot.js
@@ -7,29 +7,28 @@ const getPrediccion = require('./helpers/getPrediccion');
 
 const bot = new Twit(config);
 
-
-// async function tweetIt (txt) {  
-//     try {
-//       await bot.post('statuses/update', { status: txt });
-//       var now = new Date();
-//       console.log('TWEET SENT: '+ now.toLocaleTimeString() + '\n' + txt);
-//       console.log('------------------');
-//     } catch (err) {
-//       console.log('ERR: ' + err);
-//       console.log('------------------');
-//     }
-// };
+function tweetIt (txt) {  
+    try {
+      bot.post('statuses/update', { status: txt });
+      var now = new Date();
+      console.log('TWEET SENT: '+ now.toLocaleTimeString() + '\n' + txt);
+      console.log('------------------');
+    } catch (err) {
+      console.log('ERR: ' + err);
+      console.log('------------------');
+    }
+};
 
 //setInterval(() => tweetIt(getPrediccion()), 60000 * 120);
-//tweetIt(getPrediccion());
+tweetIt(getPrediccion());
 
-function log(){
-  console.log(getPrediccion());
-  console.log('------------------');
-}; 
+// function log(){
+//   console.log(getPrediccion());
+//   console.log('------------------');
+// }; 
 
 
-setInterval(log, 200 * 1);
+// setInterval(log, 200 * 1);
 // console.log(getPrediccion());
 // console.log('------------------');
 // console.log(getPrediccion());
